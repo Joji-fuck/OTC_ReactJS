a05d2b5e23e0fb1d089eb89add438af62b0af6ce
joji-fuck <logvmaks@gmail.com>
2025-06-23 23:42:27 +0500

    fix: images in HomePage and MappoolPage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -5,10 +5,10 @@ const HomePage = () => {
     return (
         <section className="registration">
             <a className="registration-btn" href="#">
-                <img src="./src/assets/images/OsuButton.png" alt="Запись"/>
+                <img src="../../assets/images/OsuButton.png" alt="Запись"/>
             </a>
             <div className="registration-right">
-                <img className="registration-stesha" src="./src/assets/images/Stesha.png" alt="Маскот"/>
+                <img className="registration-stesha" src="../../assets/images/Stesha.png" alt="Маскот"/>
             </div>
         </section>
     );
