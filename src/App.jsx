da3b08fe8ec59b8a334ab92a66e10d993f321801
qokori <equda.979@gmail.com>
2025-06-25 21:40:13 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import Preloader from "./components/Preloader/Preloader.jsx";
 
 import HomePage from "./pages/Home/HomePage.jsx";
 import MappoolPage from "./pages/Mappool/MappoolPage.jsx";
 import PlayersPage from "./pages/Players/PlayersPage.jsx";
 import RegulationsPage from "./pages/Regulations/RegulationPage.jsx";
-import AboutPage from "./pages/About/AboutPage.jsx";
+import StaffPage from "./pages/Staff/StaffPage.jsx";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (document.readyState === "complete") {
+      setLoading(false);
+    } else {
+      const handleLoad = () => setLoading(false);
+      window.addEventListener("load", handleLoad);
+      return () => window.removeEventListener("load", handleLoad);
+    }
+  }, []);
+
   useEffect(() => {
     const handleMouseMove = (e) => {
       const x = (e.clientX / window.innerWidth - 0.5) * 50;
@@ -27,13 +40,14 @@ function App() {
   return (
     <Router>
       <main className="App">
+        {loading && <Preloader />}
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/mappool" element={<MappoolPage />} />
           <Route path="/players" element={<PlayersPage />} />
           <Route path="/regulations" element={<RegulationsPage />} />
-          <Route path="/about" element={<AboutPage />} />
+          <Route path="/staff" element={<StaffPage />} />
         </Routes>
         <Footer />
       </main>
