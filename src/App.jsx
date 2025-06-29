fd5b1300be25ff3ddedb1fbc23f4ecee9c0ac367
joji-fuck <logvmaks@gmail.com>
2025-06-29 23:39:23 +0500

    fix: SEO

--- src/App.jsx
+++ src/App.jsx
@@ -73,17 +73,59 @@ function App() {
       <>
         <Helmet>
           <title>Osu Tyumen Cup 2025</title>
-          <meta name="description" content="Официальный сайт турнира Тюменской области по ритм-игре Osu! Расписание, игроки, маппул и регламент"/>
+          <meta name="description"
+                content="Официальный сайт турнира Тюменской области по ритм-игре Osu! Расписание, игроки, маппул и регламент"/>
+          <link rel="canonical" href="https://osutyumencup.ru/"/>
+          {/* --- Open Graph (для превью в соцсетях: ВК, Telegram) --- */}
+          <meta property="og:title" content="Osu! Tyumen Cup - Главный турнир Тюмени"/>
+          <meta property="og:description"
+                content="Вся информация о главном турнире по Osu! в Тюмени. Присоединяйтесь и следите за лучшими игроками!"/>
+          <meta property="og:image" content="https://osutyumencup.ru/social-preview.jpg"/>
+          <meta property="og:url" content="https://osutyumencup.ru/"/>
+          <meta property="og:site_name" content="Osu! Tyumen Cup"/>
+          <meta property="og:type" content="website"/>
+          {/* --- Twitter Cards (для превью в Twitter/X) --- */}
+          <meta name="twitter:card" content="summary_large_image"/>
+          <meta name="twitter:title" content="Osu! Tyumen Cup - Главный турнир Тюмени"/>
+          <meta name="twitter:description" content="Вся информация о главном турнире по Osu! в Тюмени."/>
+          <meta name="twitter:image" content="https://osutyumencup.ru/social-preview.jpg"/>
+          <script type="application/ld+json">
+            {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Event",
+              "name": "Osu! Tyumen Cup",
+              "startDate": "2025-07-05T12:00:00+05:00",
+              "endDate": "2025-07-13T20:00:00+05:00",
+              "eventStatus": "https://schema.org/EventScheduled",
+              "eventAttendanceMode": "https://schema.org/OnlineEventAttendanceMode",
+              "location": {
+                "@type": "VirtualLocation",
+                "url": "https://osutyumencup.ru/"
+              },
+              "image": [
+                "https://osutyumencup.ru/social-preview.jpg"
+              ],
+              "description": "Официальный турнир по Osu! для игроков Тюменской области.",
+              "organizer": {
+                "@type": "Organization",
+                "name": "OsuTyumenCup Staff",
+                "url": "https://osutyumencup.ru/staff"
+              }
+            }
+          `}
+          </script>
         </Helmet>
         <main className="App">
           {loading && <Preloader/>}
           <Notification/>
           <Navbar/>
-          <AnimatedRoutes />
+          <AnimatedRoutes/>
           <Donate/>
-          <Footer />
+          <Footer/>
         </main>
       </>
   );
 }
+
 export default App;
